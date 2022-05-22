c8dbfce46644eefa7aa642fdd38579ea8b52214c
William Skillas <wmskillas@gmail.com>
2022-05-21 21:26:31 -0400

    added all base files and brought over questions from my previous quiz

---
+++ js/questions.js
@@ -0,0 +1,43 @@
+var questions = [
+  {
+    question: "Where is the correct place to insert JavaScript into HTML?",
+    choices: [
+      "The <body> section",
+      "The <header> section",
+      "The <link> section",
+      "None of the above",
+    ],
+    answer: "The <body> section",
+  },
+  {
+    question: "What correlation does JavaScript have to Java?",
+    choices: [
+      "The same company, just different languages",
+      "They are the same language",
+      "Java wanted a different format",
+      "None of the above",
+    ],
+    answer: "None of the above",
+  },
+  {
+    question: "How do you insert multiline comments into Javascript?",
+    choices: ["//", "/* */", "<!-- -->", "None of the above"],
+    answer: "/* */",
+  },
+  {
+    question: "What is the correct way to write a JavaScript array?",
+    choices: [
+      "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+      "var colors = (1:'red', 2:'green', 3:'blue')",
+      "var colors = ['red', 'green', 'blue']",
+      "var colors = 'red', 'green', 'blue'",
+    ],
+    answer: "var colors = ['red', 'green', 'blue']",
+  },
+  {
+    question:
+      "What method removes the last element from an array and returns that element?",
+    choices: ["remove()", "pop()", "push()", "concat()"],
+    answer: "pop()",
+  },
+];
